c5c87e534e93e2882cdf08a968876aebc16504b3
mohameddekow <mohameddekow77@gmail.com>
2023-02-20 23:32:53 +0300

    completed js functionality

--- english dictionay/index.js
+++ english dictionay/index.js
@@ -5,27 +5,42 @@ const meaningContainerEl = document.getElementById("meaning-container")
 const titleEl = document.getElementById("title");
 const meaningEl = document.getElementById("meaning");
 
+const audioEl = document.getElementById("audio");
+
 async function fetchAPI(word) {
 
     try {
         infoTextEl.style.display = "block";
         meaningContainerEl.style.display = "none";
 
-        infoTextEl.innerHTML = `Search the meaning of ${word}!`
+        infoTextEl.innerHTML = `Search the meaning of ${word}!`;
+
         const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
 
-        const result = await fetch(url).then( (res) => res.json )
+        const result =  await fetch(url).then( (res) => res.json() );
     
-        infoTextEl.style.display = "none";
-        meaningContainerEl.style.display = "block";
-        titleEl.innerHTML = result[0].word;
-        meaningEl.innerHTML = result[0].meanings[0].definations[0].defination;
+        if(result.title){
+            meaningContainerEl.style.display = "block";
 
-        console.log(result);
+            infoTextEl.style.display = "none";
+            titleEl.innerHTML =  word;
+            meaningEl.innerHTML =  "N.A";
+            audioEl.style.display = "none";
+        }else{
+            infoTextEl.style.display = "none";
+            meaningContainerEl.style.display = "block";
 
+            audioEl.style.display = "inline-flex";
+            titleEl.innerHTML = result[0].word;
+            meaningEl.innerHTML = result[0].meanings[0].definitions[0].definition;
+            audioEl.src = result[0].phonetics[0].audio;
+        }
+
+        
 
     } catch (error) {
         console.log(error);
+        infoTextEl.innerHTML = "An error occured,please try again laiter"
     }
   
     
@@ -34,6 +49,7 @@ async function fetchAPI(word) {
 inputEl.addEventListener("keyup", (e) => {
 
     if(e.target.value && e.key == "Enter"){
+
         fetchAPI(e.target.value);
         
     }
